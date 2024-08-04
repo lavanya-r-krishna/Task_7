d050e1750fa323d9c6113d25969fe64ee4c3bc1a
lavanya <lavanya.r9611@gmail.com>
2024-08-04 19:58:17 +0530

    first commit

---
+++ src/com/task7/Demo.java
@@ -0,0 +1,9 @@
+package com.task7;
+public class Demo {
+    public static void main(String[] args) {
+        // Define the message to be printed
+        String message = "Git and Github";
+                    System.out.println(message);
+        
+    }
+}
